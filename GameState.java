2ac2345d9086843d122ab659bcf3401ae004d8ee
Sam Heil <sheil3677@gmail.com>
2017-03-25 03:39:02 -0500

    hi

---
+++ GameState.java
@@ -0,0 +1,29 @@
+
+public class GameState {
+	final int maxDelay = 10000, delayConst = 100000000;
+	private int points, lives;
+	private long lastRestart;
+	public GameState(int nlives) {
+		lives = nlives;
+		lastRestart = System.currentTimeMillis();
+	}
+	public boolean die() {
+		--lives;
+		if (lives == 0)
+			return true;
+		lastRestart = System.currentTimeMillis();
+		return false;
+	}
+	public void score(int numPts) {
+		points += numPts;
+	}
+	public int getScore() {
+		return points;
+	}
+	public int getLives() {
+		return lives;
+	}
+	public int getShapeDelay() {
+		return (int)Math.min(maxDelay, 1.0*delayConst/lastRestart);
+	}
+}
